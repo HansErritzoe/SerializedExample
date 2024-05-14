1d95b2d1e93d631d1a8baaeb8781a655c8483bcd
HansErritzoe <h.erritzoe@hotmail.com>
2024-05-14 22:01:52 +0200

    smol update

--- src/MemberRepository.java
+++ src/MemberRepository.java
@@ -5,7 +5,8 @@ public class MemberRepository {
 
     // Static method to write an ArrayList of Member objects to a file
     public static void writeMembers(ArrayList<Member> members, String filePath) {
-        try (ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(filePath))) {
+        try {
+            ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(filePath));
             for (Member member : members) {
                 oos.writeObject(member); // Serialize each Member object and write to the file
             }
@@ -17,7 +18,8 @@ public class MemberRepository {
     // Static method to read Member objects from a file into an ArrayList
     public static ArrayList<Member> readMembers(String filePath) {
         ArrayList<Member> members = new ArrayList<>();
-        try (ObjectInputStream ois = new ObjectInputStream(new FileInputStream(filePath))) {
+        try {
+            ObjectInputStream ois = new ObjectInputStream(new FileInputStream(filePath));
             while (true) {
                 try {
                     Member member = (Member) ois.readObject(); // Deserialize a Member object from the file
